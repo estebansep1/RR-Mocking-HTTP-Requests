409bac9b7b36c2cda7496bcbecf10a64079a0025
Esteban Sepulveda <tebansd@icloud.com>
2023-10-01 13:41:20 -0700

    final solution

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
-  return (
+const [gitHubData, setGitHubData] = useState({})
+
+
+useEffect(() => {
+    fetch('https://api.github.com/users/estebansep1')
+    .then(res => res.json())
+    .then(data => {
+        setGitHubData(data)
+    })
+}, [])
+
+return (
     <div className="App">
-      <h1>Github Profile Info:</h1>
+        <h1>Github Profile Info:</h1>
+        <h2>{gitHubData.login}</h2>
+        <a href={gitHubData.html_url}><button>Link to GitHub profile</button></a>
     </div>
-  );
+);
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,25 @@
+import { render, screen, waitFor } from '@testing-library/react'
+import App from './App'
 
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+})
+
+describe('test for the GitHub API request', () => {
+    test('header text shows GitHub login name', async () => {
+        fetch.mockResponseOnce(JSON.stringify({login: 'estebansep1'}))
+        render(<App />)
+        const headerEl = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+        expect(headerEl.textContent).toBe('estebansep1')
+    })
+
+    test('button links to correct GitHub profile', async () => {
+        fetch.mockResponseOnce(JSON.stringify({html_url: "https://github.com/estebansep1"}))
+        render(<App />)
+        const linkEl = await waitFor(() => screen.getByRole('link'))
+        expect(linkEl.href).toBe("https://github.com/estebansep1",)
+    })
+})
 
 
